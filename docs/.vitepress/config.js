85fbb48646452fe67e48897bc8b5bd1be992cee6
limoxi <asia-aixiang@163.com>
2024-09-15 16:10:38 +0800

    update

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -15,7 +15,7 @@ export default defineConfig({
 
 
         socialLinks: [
-            {icon: 'github', link: 'https://github.com/vuejs/vitepress'}
+            {icon: 'github', link: 'https://github.com/limoxi/teamdo'}
         ]
     }
 })
