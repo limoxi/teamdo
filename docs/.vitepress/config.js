9e185bba5ca30c84205647cf5acb60e60e52acaa
limoxi <asia-aixiang@163.com>
2024-09-15 14:23:21 +0800

    update

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -3,6 +3,7 @@ import {defineConfig} from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
     title: "TeamDo",
+    base: "/teamdo/",
     description: "Docs of TeamDo",
     appearance: 'force-dark',
     themeConfig: {
