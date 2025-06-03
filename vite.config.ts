c64e75f1d2196b6a62d497931ca15e76783b40b4
limoxi <asia-aixiang@163.com>
2025-06-03 23:49:54 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,38 +4,38 @@ import vue from '@vitejs/plugin-vue'
 const envPrefix = 'APP_'
 
 export default defineConfig({
-  base: '/',
-  plugins: [
-    vue()
-  ],
-  envPrefix,
-  resolve: {
-    alias: {
-      '@': '/src'
+    base: '/',
+    plugins: [
+        vue(),
+    ],
+    envPrefix,
+    resolve: {
+        alias: {
+            '@': '/src'
+        },
+        extensions: ['.js', '.vue', '.json', '.less', '.css']
     },
-    extensions: ['.js', '.vue', '.json', '.less', '.css']
-  },
-  css: {
-    preprocessorOptions: {
-      less: {
-        javascriptEnabled: true
-      }
+    css: {
+        preprocessorOptions: {
+            less: {
+                javascriptEnabled: true
+            }
+        }
+    },
+    server: {
+        host: '127.0.0.1',
+        port: 3000,
+        open: true,
+        proxy: {
+            // '/iteamdo': {  // 使用远端服务时使用
+            //     target: 'http://api.teamdo.com',
+            //     changeOrigin: true
+            // },
+            '/iteamdo': {  // 本地启动服务端时使用
+                target: 'http://127.0.0.1:8341',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/iteamdo/, '')
+            }
+        }
     }
-  },
-  server: {
-    host: '127.0.0.1',
-    port: 3000,
-    open: true,
-    proxy: {
-      // '/iteamdo': {  // 使用远端服务时使用
-      //     target: 'http://api.teamdo.com',
-      //     changeOrigin: true
-      // },
-      '/iteamdo': {  // 本地启动服务端时使用
-        target: 'http://127.0.0.1:8341',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/iteamdo/, '')
-      }
-    }
-  }
 })
